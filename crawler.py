64075738b55af99153fc2648e4905f1c93099b20
Alperen <alperenmac21@icloud.com>
2021-02-10 13:58:32 +0300

    created SponsorshipCrawler class

--- crawler.py
+++ crawler.py
@@ -3,17 +3,28 @@ import requests
 from selenium import webdriver
 import time
 from selenium.webdriver.firefox.options import Options
+from EnvLoginGatherer import EnvLoginGatherer
 
 
-options = Options()
-options.add_argument('-headless')
+class SponsorshipCrawler(EnvLoginGatherer):
 
-driver = webdriver.Firefox(
-    executable_path=r'/Users/alperen/Desktop/geckodriver', options=options)
-driver.get('https://www.instagram.com')
+    def __init__(self, json_directory="login.json", gecko_directory=r'/Users/alperen/Desktop/geckodriver'):
+        options = Options()
+        options.add_argument('-headless')
+        self.driver = webdriver.Firefox(
+            executable_path=gecko_directory, options=options)
+        super().__init__(json_directory)
 
-time.sleep(3)
-email = driver.find_element_by_xpath(
-    "//*[@id='loginForm']/div/div[1]/div/label/input")
+    def instagram(self):
+        self.driver.get('https://www.instagram.com')
+        time.sleep(3)
+        email = self.driver.find_element_by_xpath(
+            "//*[@id='loginForm']/div/div[1]/div/label/input")
 
-driver.close()
+    def close(self):
+        self.driver.close()
+
+
+crawler = SponsorshipCrawler()
+crawler.instagram()
+crawler.close()
